7c7b1671cf38bcd51b7a7b145ef03b3624d51ee0
kk5 <klaudiusz.kolegowicz@softnet.com.pl>
2023-07-17 15:48:47 +0200

    fix list

--- Wsei-Backend-UserManaging/app.py
+++ Wsei-Backend-UserManaging/app.py
@@ -120,10 +120,8 @@ class UserList(Resource):
 
             # Execute the SELECT statement to retrieve all users with their roles
             cursor.execute("""
-                SELECT u.id, u.first_name, u.last_name, u.username, u.deleted, r.role
-                FROM users u
-                JOIN user_roles ur ON u.id = ur.user_id
-                JOIN roles r ON ur.role_id = r.id
+                SELECT id, first_name, last_name, username, deleted
+                FROM users
             """)
 
             # Fetch all rows from the cursor
@@ -138,7 +136,7 @@ class UserList(Resource):
                     'last_name': row[2],
                     'username': row[3],
                     'deleted': row[4],
-                    'role': row[5]
+   
                 }
                 users.append(user)
 
